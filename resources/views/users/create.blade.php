aa4c1aad84962c84089c4cabe580f4b23656d66b
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-04-22 11:11:52 +0800

    Added <x-form.error> component

--- resources/views/users/create.blade.php
+++ resources/views/users/create.blade.php
@@ -15,21 +15,21 @@
                         <x-jet-label for="name" value="{{ __('Name') }}" />
                         <x-jet-input id="name" class="block mt-1 w-full" type="text" name="name" :value="old('name')"
                             required autofocus autocomplete="name" />
+                        <x-form.error name="name" />
                     </div>
 
                     <div class="mt-4">
                         <x-jet-label for="email" value="{{ __('Email') }}" />
                         <x-jet-input id="email" class="block mt-1 w-full" type="email" name="email" :value="old('email')"
                             required />
+                        <x-form.error name="email" />
                     </div>
 
                     <div class="mt-4">
                         <x-jet-label for="password" value="{{ __('Password') }}" />
                         <x-jet-input id="password" class="block mt-1 w-full" type="password" name="password" required
                             autocomplete="new-password" />
-                            @error('password')
-                            <p class="text-red-600 text-sm">{{ $message }}</p>
-                        @enderror
+                        <x-form.error name="password" />
                     </div>
 
                     <div class="mt-4">
