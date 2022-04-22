2d2bf6bf5fa00871dfbff49150eef12549c375df
Nasrul Hazim Bin Mohamad <nasrulhazim.m@gmail.com>
2022-04-22 08:51:48 +0800

    Compiled FE Asset

--- resources/views/users/index.blade.php
+++ resources/views/users/index.blade.php
@@ -7,15 +7,30 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg px-4 py-4">
-                {{ $users->links() }}
-                <ul>
+            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg py-4">
+                <x-table title="Users" description="List of users in this application" newRoute="{{ route('users.create') }}">
+                    <x-slot:header>
+                        <x-table.th>
+                            Name
+                        </x-table.th>
+                        <x-table.th>
+                            E-mail
+                        </x-table.th>
+                    </x-slot:header>
                     @foreach ($users as $user)
-                        <li class="px-4 py-4">{{ $user->name }}</li>
+                        <tr>
+                            <x-table.td>
+                                {{ $user->name }}
+                            </x-table.td>
+                            <x-table.td>
+                                {{ $user->email }}
+                            </x-table.td>
+                        </tr>
                     @endforeach
-                </ul>
-                {{ $users->links() }}
-            </div>
+                    <x-slot:footer>
+                        {{ $users->links() }}
+                    </x-slot:footer>
+                </x-table>
         </div>
     </div>
 </x-app-layout>
